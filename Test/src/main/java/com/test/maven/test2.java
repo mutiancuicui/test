1755fe1162bda5ed3dfa59b5fc3153acb21858b0
TIANHONGYONG <TIANHONGYONG@DESKTOP-ROBEO89>
2024-07-03 21:58:46 +0800

    111

---
+++ Test/src/main/java/com/test/maven/test2.java
@@ -0,0 +1,9 @@
+package com.test.maven;
+
+public class test2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Truck can carry  pounds222.");
+
+	}
+}
