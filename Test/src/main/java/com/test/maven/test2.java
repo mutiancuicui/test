2da778c086c44f359617199c90df3faffcf49244
tianhongyong <15987815447@163.com>
2024-07-04 02:16:01 +0800

    111

--- Test/src/main/java/com/test/maven/test2.java
+++ Test/src/main/java/com/test/maven/test2.java
@@ -3,7 +3,7 @@ package com.test.maven;
 public class test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Truck can carry  pounds.");
+		System.out.println("Truck can carry  pounds1.");
 
 	}
 }
