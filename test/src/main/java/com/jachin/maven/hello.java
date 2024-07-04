93b16a19ff33676015dcbb08eaa5ef71bae74ac1
tianhongyong <15987815447@163.com>
2024-07-04 12:16:17 +0800

    111

--- test/src/main/java/com/jachin/maven/hello.java
+++ test/src/main/java/com/jachin/maven/hello.java
@@ -4,7 +4,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Truck can carry  pounds.");
-		System.out.println("Truck can carry  pounds.111");
+		System.out.println("Truck can carry  pounds.11");
 		System.out.println("Truck can carry  pounds.222");
 		System.out.println("Truck can carry  pounds.333");
 	}
