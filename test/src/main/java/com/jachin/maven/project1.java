a307b13544a3bcb3833dae6a587ca1d006d1304c
tianhongyong <15987815447@163.com>
2024-07-04 13:18:55 +0800

    111

--- test/src/main/java/com/jachin/maven/project1.java
+++ test/src/main/java/com/jachin/maven/project1.java
@@ -3,7 +3,7 @@ package com.jachin.maven;
 public class project1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Truck can carry  pounds11.");
+		System.out.println("Truck can carry  pounds1.");
 
 	}
 }
